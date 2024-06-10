aa7313a484b24ddc94710659d609e683e5e1e737
Nishant Bisht <116999283+a3s0h@users.noreply.github.com>
2024-06-10 20:07:16 +0530

    added the deployed base url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,3 @@
 export const HeaderImage = "https://www.svgrepo.com/show/444890/comment.svg"
-// export const BASE_URL = "https://blog-full-stack-stkr.onrender.com/"
-export const BASE_URL = "http://localhost:3000/"
+export const BASE_URL = "https://bloger-backend.onrender.com/"
+// export const BASE_URL = "http://localhost:3000/"
